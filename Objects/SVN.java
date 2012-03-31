fa11820a717e3a9435358f1619f7951d755b9672
Abdelrahman Kamel <ak883@cornell.edu>
2012-03-30 22:37:38 -0400

    adding svn?

---
+++ Objects/SVN.java
@@ -0,0 +1,31 @@
+package Objects;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+
+public class SVN implements Serializable{
+	
+	private static final long serialVersionUID = -6264611582958483765L;
+	private int changeCount;
+	
+	public int getChangeCount() {
+		return changeCount;
+	}
+	public InetAddress getIpPrime() {
+		return ipPrime;
+	}
+	public InetAddress getIpBackup() {
+		return ipBackup;
+	}
+	public void setChangeCount(int changeCount) {
+		this.changeCount = changeCount;
+	}
+	public void setIpPrime(InetAddress ipPrime) {
+		this.ipPrime = ipPrime;
+	}
+	public void setIpBackup(InetAddress ipBackup) {
+		this.ipBackup = ipBackup;
+	}
+	private InetAddress ipPrime;
+	private InetAddress ipBackup;
+}
