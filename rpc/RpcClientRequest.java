e662ad5be1978da867b53da945cb0b2412bf6a2c
Jonathan Pullano <jp938@cornell.edu>
2012-03-31 14:29:45 -0400

    RPC Updates

---
+++ rpc/RpcClientRequest.java
@@ -0,0 +1,77 @@
+package rpc;
+
+import identifiers.IPP;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
+
+import rpc.message.RpcMessage;
+import rpc.message.RpcMessageCall;
+import rpc.message.RpcMessageReply;
+
+public class RpcClientRequest extends Thread {
+    private ArrayList<IPP> ippList;
+    private int opCode;
+    private ArrayList<Object> arguments;
+    private ArrayList<Object> results = null;
+    
+    public RpcClientRequest(ArrayList<IPP> ippList, int opCode, ArrayList<Object> arguments) {
+        this.ippList = ippList;
+        this.opCode = opCode;
+        this.arguments = arguments;
+    }
+    
+    @Override
+    public void run() {
+        int callID = RpcServer.getInstance().callID();
+
+        DatagramSocket rpcSocket = null;
+        try {
+            rpcSocket = new DatagramSocket();
+        } catch (SocketException e1) {
+            System.err.println("Failed to create socket");
+            e1.printStackTrace();
+        }
+        RpcMessageCall outMsg = new RpcMessageCall(callID, opCode, arguments);
+        byte[] outBuf = outMsg.toByteStream();
+        if(outBuf.length > RpcMessage.BUFFER_SIZE) {
+            throw new RuntimeException("Message Exceeds Maximum Packet Length");
+        }
+        for( IPP address : ippList ) {
+            DatagramPacket sendPkt = new DatagramPacket(outBuf, RpcMessage.BUFFER_SIZE, address.getIp(), address.getPort());
+            try {
+                rpcSocket.send(sendPkt);
+            } catch (IOException e) {
+                // TODO: Smarty smart stuff
+                e.printStackTrace();
+            }
+        }
+        byte [] inBuf = new byte[RpcMessage.BUFFER_SIZE];
+        DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
+        RpcMessageReply inMsg = null;
+        try {
+            do {
+                recvPkt.setLength(inBuf.length);
+                rpcSocket.receive(recvPkt);
+                inMsg = (RpcMessageReply) RpcMessage.readByteStream(inBuf);
+            } while( inMsg != null && inMsg.getCallID() != callID );
+        } catch(InterruptedIOException iioe) {
+            // timeout
+            inMsg = null;
+        } catch(IOException ioe) {
+            // other error
+        }
+        results = inMsg.getResults();
+    }
+    
+    /**
+     * Returns the result of the request, or NULL if the request has not been completed
+     */
+    public ArrayList<Object> getResults() {
+        return results;
+    }
+}
